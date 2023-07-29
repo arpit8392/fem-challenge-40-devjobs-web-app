c3d20603d8a7300d1976e8fa59a833759609feb2
Arpit Namdev <arpit8392@gmail.com>
2023-07-30 00:18:41 +0530

    Dark Mode | Base Configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Kumbh_Sans } from 'next/font/google'
+import ThemeProvider from '@/components/ThemeProvider'
 
 const kumbh_sans = Kumbh_Sans({ subsets: ['latin'] })
 
@@ -17,7 +18,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={kumbh_sans.className}>{children}</body>
+			<body className={kumbh_sans.className}>
+				<ThemeProvider enableSystem={true} attribute='class'>
+					<main>{children}</main>
+				</ThemeProvider>
+			</body>
 		</html>
 	)
 }
