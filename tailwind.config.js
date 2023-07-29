db9885740ea1edb6a10c85bd7df35a7782d58d06
Arpit Namdev <arpit8392@gmail.com>
2023-07-30 00:11:16 +0530

    Base Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				violet: '#9e7f66',
+				lightViolet: '#939BF4',
+				veryDarkBlue: '#19202D',
+				midnight: '#121721',
+				lightGrey: '#F4F6F8',
+				gray: '#9DAEC2',
+				darkGrey: '#6E8098',
+			},
+		},
+	},
+	plugins: [],
 }
